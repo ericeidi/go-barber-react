d42ce0a938b0c7854e4677b71c3e479ebe863cf1
Eric <ericen01@gmail.com>
2020-09-16 17:52:51 -0300

    improving ux in sign in and sign up layout

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,5 +1,10 @@
-import React from 'react';
-import { FiMail, FiUser, FiLock, FiLogIn } from 'react-icons/fi';
+import React, {useRef, useCallback} from 'react';
+import { FiMail, FiLock, FiLogIn } from 'react-icons/fi';
+import { Form } from '@unform/web';
+import { FormHandles } from '@unform/core';
+import * as Yup from 'yup';
+
+import getValidationErrors from '../../utils/getValidationErrors'
 
 import logoImg from '../../assets/logo.svg';
 
@@ -9,17 +14,44 @@ import Input from '../../components/Input';
 
 import Button from '../../components/Button';
 
-const SignUp: React.FC = () => (
+const SignIn: React.FC = () => {
+
+  const formRef = useRef<FormHandles>(null);
+
+
+
+  const handleSubmit = useCallback(async (data: object) =>{
+    try{
+      formRef.current?.setErrors({});
+
+      const schema = Yup.object().shape({
+        name: Yup.string().required('Name is required'),
+        email: Yup.string().required('E-mail is required').email('Enter a valid e-mail'),
+        password: Yup.string().required('Password is required'),
+      });
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+
+    }catch(err){
+      const errors = getValidationErrors(err);
+      formRef.current?.setErrors(errors)
+    }
+  }, []);
+
+  return (
+
   <Container>
     <Content>
       <img src={logoImg} alt="GoBarber" />
-      <form>
+      <Form ref={formRef} onSubmit={handleSubmit}>
         <h1>Please login</h1>
 
         <Input name="email" icon={FiMail} placeholder="E-mail"/>
         <Input name="password" icon={FiLock} type="password" placeholder="Password" />
         <Button type="submit">Log in</Button>
-      </form>
+      </Form>
 
       <a href="login">
         <FiLogIn/>
@@ -30,5 +62,5 @@ const SignUp: React.FC = () => (
 
   </Container>
 );
-
-export default SignUp
+};
+export default SignIn
--- src/pages/SignUp/index.tsx
+++ src/pages/SignUp/index.tsx
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, { useCallback, useRef} from 'react';
 import { FiArrowLeft, FiUser, FiMail, FiLock } from 'react-icons/fi';
+import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
+import * as Yup from 'yup';
+import getValidationErrors from '../../utils/getValidationErrors';
 
 import logoImg from '../../assets/logo.svg';
 
@@ -10,11 +13,31 @@ import Input from '../../components/Input';
 
 import Button from '../../components/Button';
 
-const SignIn: React.FC = () => {
+const SignUp: React.FC = () => {
 
-  function handleSubmit(data: object): void{
-    console.log(data)
-  }
+  const formRef = useRef<FormHandles>(null);
+
+
+
+  const handleSubmit = useCallback(async (data: object) =>{
+    try{
+      formRef.current?.setErrors({});
+
+      const schema = Yup.object().shape({
+        name: Yup.string().required('Name is required'),
+        email: Yup.string().required('E-mail is required').email('Enter a valid e-mail'),
+        password: Yup.string().required('Password is required').min(6, 'Minimum of 6 digits'),
+      });
+
+      await schema.validate(data, {
+        abortEarly: false,
+      });
+
+    }catch(err){
+      const errors = getValidationErrors(err);
+      formRef.current?.setErrors(errors)
+    }
+  }, []);
   return (
 
 
@@ -22,7 +45,7 @@ const SignIn: React.FC = () => {
       <Background />
       <Content>
         <img src={logoImg} alt="GoBarber" />
-        <Form onSubmit={handleSubmit}>
+        <Form ref={formRef} onSubmit={handleSubmit}>
           <h1>Sign up</h1>
 
           <Input name="name" icon={FiUser} placeholder="Name"/>
@@ -43,4 +66,4 @@ const SignIn: React.FC = () => {
   );
 }
 
-export default SignIn
+export default SignUp
