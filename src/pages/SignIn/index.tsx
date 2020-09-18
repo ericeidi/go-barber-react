fab3d8db34fec5f3164f85a4afe0bb4173c73b13
Eric <ericen01@gmail.com>
2020-09-18 13:46:17 -0300

    finalizing authentication

--- src/pages/SignIn/index.tsx
+++ src/pages/SignIn/index.tsx
@@ -1,10 +1,11 @@
-import React, {useRef, useCallback, useContext} from 'react';
+import React, {useRef, useCallback } from 'react';
 import { FiMail, FiLock, FiLogIn } from 'react-icons/fi';
 import { Form } from '@unform/web';
 import { FormHandles } from '@unform/core';
 import * as Yup from 'yup';
 
-import { AuthContext } from '../../context/AuthContext';
+
+import { useAuth } from '../../hooks/AuthContext';
 import getValidationErrors from '../../utils/getValidationErrors'
 
 import logoImg from '../../assets/logo.svg';
@@ -15,15 +16,20 @@ import Input from '../../components/Input';
 
 import Button from '../../components/Button';
 
+interface SignInFormData {
+  email: string;
+  password: string;
+}
+
 const SignIn: React.FC = () => {
 
   const formRef = useRef<FormHandles>(null);
 
-  const { name } = useContext(AuthContext);
+  const { user, signIn } = useAuth();
 
-  console.log(name);
+  console.log(user);
 
-  const handleSubmit = useCallback(async (data: object) =>{
+  const handleSubmit = useCallback(async (data: SignInFormData) =>{
     try{
       formRef.current?.setErrors({});
 
@@ -33,15 +39,24 @@ const SignIn: React.FC = () => {
         password: Yup.string().required('Password is required'),
       });
 
+
+      signIn({
+        email: data.email,
+        password: data.password,
+      });
+
       await schema.validate(data, {
         abortEarly: false,
       });
+
+
+
 
     }catch(err){
       const errors = getValidationErrors(err);
       formRef.current?.setErrors(errors)
     }
-  }, []);
+  }, [signIn]);
 
   return (
 
